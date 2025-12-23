c9f2277055d7863780657b28ef6baf3d894de51c
aboodsalameh2006 <abodsalameh2006@gmail.com>
2025-12-23 21:02:00 +0200

    Update CardElectricPage.jsx

--- src/components/CardElectricPage.jsx
+++ src/components/CardElectricPage.jsx
@@ -3,25 +3,25 @@ import "./CardElectricPage.css";
 export default function CardElectricPage({ img, carName, specs }) {
   return (
     <div className="e-card">
-      <div className="e-cardimg">
+      <div className="e-card__img">
         {img ? (
           <img src={img} alt={carName} />
         ) : (
-          <div className="e-cardimgPlaceholder">No Image</div>
+          <div className="e-card__imgPlaceholder">No Image</div>
         )}
       </div>
 
-      <div className="e-cardbody">
-        <h3 className="e-cardtitle">{carName}</h3>
+      <div className="e-card__body">
+        <h3 className="e-card__title">{carName}</h3>
 
         {specs && specs.length > 0 ? (
-          <ul className="e-cardlist">
+          <ul className="e-card__list">
             {specs.map((item, index) => (
               <li key={index}>{item}</li>
             ))}
           </ul>
         ) : (
-          <p className="e-cardcoming">Coming Soon...</p>
+          <p className="e-card__coming">Coming Soon...</p>
         )}
       </div>
     </div>
